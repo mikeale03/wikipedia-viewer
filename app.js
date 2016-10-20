0cb9232498bde0bc4d8b19ffaeea89794c5d963f
mikeale03 <mikeale03@gmail.com>
2016-10-20 21:25:19 +0800

    click to search adding no match found

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 angular.module("MyApp",["ngAnimate"] )
   .controller("myController", function($http) {
   var ctrl = this;
+  ctrl.noMatch = false;
   var api ="https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&meta=&titles=&generator=search&exsentences=1&exlimit=20&exintro=1&explaintext=1&gsrsearch=";
   var cb = "&callback=JSON_CALLBACK";
   var url = "https://en.wikipedia.org/?curid=";
 
   ctrl.submitSearch = function() {
+    document.getElementById('input').focus();
     if(ctrl.search) {
       ctrl.wikiPages = [];
       $http.jsonp(api +ctrl.search+ cb)
       .success(function(data) {
+        if(data.hasOwnProperty("query")) {
         var pages = data.query.pages;
-        angular.forEach(pages, function(value,key)  {
-          ctrl.wikiPages.push({title: value.title, body: value.extract, link: url+value.pageid })
-        });
+          ctrl.noMatch = false;
+          angular.forEach(pages, function(value,key)  {
+            ctrl.wikiPages.push({title: value.title, body: value.extract, link: url+value.pageid })
+          });
+        } else {
+          ctrl.noMatch = true;
+        }
       });
       ctrl.search = "";
     }
